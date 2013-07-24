2f6e605aaa978a0418e0efa10f84f591764a0132
Apprentice <apprentice@devbootcamp.com>
2013-07-24 16:09:15 -0700

    added remove field links

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
-  // $("#new-survey").on('click', function(e){
-  //     e.preventDefault();
-  //       $.get()
-  // });
-});
+function remove_fields(link) {
+    $(link).prev("input[type=hidden]").value = '1';
+    $(link).closest(".fields").hide();
+}
+
+function add_fields(link) {
+  $(link).closest(".field").prepend("inpute[type=text_field]");
+}
